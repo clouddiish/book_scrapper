bd75f9382c3e759db6f7b335d7a414a0d337182e
Olga Szymczak <140915825+clouddiish@users.noreply.github.com>
2025-01-27 22:18:11 +0100

    improved readabiity

--- books_scrapper.py
+++ books_scrapper.py
@@ -4,49 +4,47 @@ from bs4 import BeautifulSoup
 
 
 def get_urls():
-    urls = []
-    for page in range(1, 7):
-        urls.append(
-            f"https://books.toscrape.com/catalogue/category/books/nonfiction_13/page-{page}.html"
-        )
-
-    return urls
+    base_url = (
+        "https://books.toscrape.com/catalogue/category/books/nonfiction_13/page-{}.html"
+    )
+    return [base_url.format(page) for page in range(1, 7)]
 
 
-def get_book_cards_from_one_page(url):
+def fetch_book_cards_from_page(url):
     page = requests.get(url)
     soup = BeautifulSoup(page.content, "html.parser")
     results = soup.find("div", class_="col-sm-8 col-md-9")
-    book_cards = results.find_all("article", class_="product_pod")
-    return book_cards
+    return results.find_all("article", class_="product_pod")
 
 
-def get_book_cards_from_all_pages(urls):
+def fetch_all_book_cards(urls):
     book_cards_list = []
     for url in urls:
-        book_cards_list += get_book_cards_from_one_page(url)
+        book_cards_list += fetch_book_cards_from_page(url)
     return book_cards_list
 
 
-def write_books_data(book_cards_list, filename):
+def extract_book_data(book_card):
+    title = book_card.find_all("a")[1].text.strip()
+    rating = book_card.find("p", class_="star-rating")["class"][1]
+    price = book_card.find("p", class_="price_color").text.strip("£")
+
+    return title, rating, price
+
+
+def write_books_to_csv(book_cards, filename):
     with open(filename, "w", newline="") as f:
         writer = csv.writer(f)
         writer.writerow(["title", "rating", "price"])
 
-        for book_card in book_cards_list:
-            title_element = book_card.find_all("a")[1]
-            rating_element = book_card.find("p", class_="star-rating")
-            price_element = book_card.find("p", class_="price_color")
-
-            title = title_element.text.strip()
-            rating = rating_element["class"][1]
-            price = price_element.text.strip().strip("£")
-
-            writer.writerow([title, rating, price])
+        for book_card in book_cards:
+            writer.writerow(extract_book_data(book_card))
 
 
 def main():
-    write_books_data(get_book_cards_from_all_pages(get_urls()), "books.csv")
+    urls = get_urls()
+    book_cards = fetch_all_book_cards(urls)
+    write_books_to_csv(book_cards, "books.csv")
 
 
 if __name__ == "__main__":
