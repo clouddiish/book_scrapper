1e12b59c5110e8388a282d8b515cd0bd15afaed4
Olga Szymczak <140915825+clouddiish@users.noreply.github.com>
2025-01-27 21:37:55 +0100

    small refactor

--- books_scrapper.py
+++ books_scrapper.py
@@ -1,23 +1,37 @@
 import requests
+import csv
 from bs4 import BeautifulSoup
 
 URL = "https://books.toscrape.com/catalogue/category/books/nonfiction_13/index.html"
-page = requests.get(URL)
+FILE = "books.csv"
 
-soup = BeautifulSoup(page.content, "html.parser")
 
-results = soup.find("div", class_="col-sm-8 col-md-9")
-book_cards = results.find_all("article", class_="product_pod")
+def get_book_cards(url):
+    page = requests.get(URL)
+    soup = BeautifulSoup(page.content, "html.parser")
+    results = soup.find("div", class_="col-sm-8 col-md-9")
+    book_cards = results.find_all("article", class_="product_pod")
+    return book_cards
 
-for book_card in book_cards:
-    title_element = book_card.find_all("a")[1]
-    rating_element = book_card.find("p", class_="star-rating")
-    price_element = book_card.find("p", class_="price_color")
 
-    title = title_element.text.strip()
-    rating = rating_element["class"][1]
-    price = price_element.text.strip()
+def print_books_data(book_cards):
+    for book_card in book_cards:
+        title_element = book_card.find_all("a")[1]
+        rating_element = book_card.find("p", class_="star-rating")
+        price_element = book_card.find("p", class_="price_color")
 
-    print(title)
-    print(rating)
-    print(price)
+        title = title_element.text.strip()
+        rating = rating_element["class"][1]
+        price = price_element.text.strip()
+
+        print(title)
+        print(rating)
+        print(price)
+
+
+def main():
+    print_books_data(get_book_cards(URL))
+
+
+if __name__ == "__main__":
+    main()
