935bb39624f7c5e289ef5c946d4ad406a83a962a
Olga Szymczak <140915825+clouddiish@users.noreply.github.com>
2025-01-30 20:02:33 +0100

    http error

--- async_books_scrapper.py
+++ async_books_scrapper.py
@@ -5,19 +5,30 @@ import csv
 from bs4 import BeautifulSoup
 
 
+class HttpError(Exception):
+    def __init__(self, message, error_code):
+        super().__init__(message)
+        self.error_code = error_code
+
+
 def get_urls():
     base_url = (
-        "https://books.toscrape.com/catalogue/category/books/nonfiction_13/page-{}.html"
+        "https://books.toscrape.com/catalogue/category/boks/nonfiction_13/page-{}.html"
     )
     return [base_url.format(page) for page in range(1, 7)]
 
 
 async def fetch_book_cards_from_page(session, url):
-    async with session.get(url) as response:
-        page = await response.read()
-        soup = BeautifulSoup(page, "html.parser")
-        results = soup.find("div", class_="col-sm-8 col-md-9")
-        return results.find_all("article", class_="product_pod")
+    try:
+        async with session.get(url) as response:
+            if not response.ok:
+                raise HttpError("Http error occured", response.status)
+            page = await response.read()
+            soup = BeautifulSoup(page, "html.parser")
+            results = soup.find("div", class_="col-sm-8 col-md-9")
+            return results.find_all("article", class_="product_pod")
+    except HttpError as http_err:
+        print("Http error occured with code: ", http_err.error_code)
 
 
 async def fetch_all_book_cards(urls):
