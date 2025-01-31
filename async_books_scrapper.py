217232d4c7e9ca513da2e60f93c48a3d61b49674
Olga Szymczak <140915825+clouddiish@users.noreply.github.com>
2025-01-31 16:45:58 +0100

    corrected docstrings

--- async_books_scrapper.py
+++ async_books_scrapper.py
@@ -22,9 +22,9 @@ class HttpError(Exception):
     """
     Custom exception to handle HTTP errors during web scraping.
 
-    Args:
+    Attributes:
         message (str): The error message.
-        error_code (int): The HTTP error status code.
+        error_code (int): The HTTP status code.
     """
 
     def __init__(self, message, error_code):
@@ -34,10 +34,10 @@ class HttpError(Exception):
 
 async def fetch_number_of_pages():
     """
-    Fetch number of pages on the site to scrap from.
+    Fetches the total number of pages available for scraping.
 
     Returns:
-        int: Number of pages on the site to scrap from.
+        int: The total number of pages on the website.
     """
     url = "https://books.toscrape.com"
 
@@ -46,7 +46,7 @@ async def fetch_number_of_pages():
             async with session.get(url) as response:
                 if not response.ok:
                     raise HttpError(
-                        f"Http error occured in url {url} with code {response.status}",
+                        f"HTTP error occurred while accessing {url} (Status Code: {response.status})",
                         response.status,
                     )
 
@@ -54,32 +54,31 @@ async def fetch_number_of_pages():
                 soup = BeautifulSoup(page, "html.parser")
 
                 results = soup.find("li", class_="current")
-                logger.info("Found maximum number of pages")
+                logger.info("Found maximum number of pages.")
                 return int(results.text.split()[3])
     except Exception as error:
-        logger.error(f"Unable to fetch number of pages, {error}")
+        logger.error(f"Unable to fetch the number of pages: {error}")
 
 
 async def get_urls():
     """
-    Generate a list of URLs to scrape book data from.
+    Generates a list of URLs for all pages containing book listings.
 
     Returns:
-        list: A list of formatted URLs for different pages of books.
+        list: A list of URLs to scrape.
     """
     try:
         base_url = "https://books.toscrape.com/catalogue/page-{}.html"
         number_of_pages = await fetch_number_of_pages()
-        logger.info("Getting list of URLs")
+        logger.info("Generating list of URLs.")
         return [base_url.format(page) for page in range(1, number_of_pages + 1)]
-
     except Exception as error:
-        logger.error(f"Unable get list of URLs, {error}")
+        logger.error(f"Unable to generate list of URLs: {error}")
 
 
 async def fetch_book_cards_from_page(session, sem, url):
     """
-    Fetch book cards from a single page and parse the HTML.
+    Fetches book cards from a single page.
 
     Args:
         session (aiohttp.ClientSession): The HTTP session for making requests.
@@ -87,16 +86,15 @@ async def fetch_book_cards_from_page(session, sem, url):
         url (str): The URL of the page to scrape.
 
     Returns:
-        list: A list of book card elements parsed from the HTML.
-        If no results are found, returns an empty list.
+        list: A list of BeautifulSoup elements representing book cards.
+        Returns an empty list if no books are found or an error occurs.
     """
     async with sem:
         try:
             async with session.get(url) as response:
-
                 if not response.ok:
                     raise HttpError(
-                        f"Http error occured in url {url} with code {response.status}",
+                        f"HTTP error occurred while accessing {url} (Status Code: {response.status})",
                         response.status,
                     )
 
@@ -108,68 +106,62 @@ async def fetch_book_cards_from_page(session, sem, url):
                     return []
 
                 return results.find_all("article", class_="product_pod")
-
         except HttpError as http_err:
             logger.error(
-                f"Unable to fetch book cards from page {url}. Http error occured with code {http_err.error_code}"
+                f"HTTP error while fetching book cards from {url}: {http_err.error_code}"
             )
             return []
 
 
 async def fetch_all_book_cards(urls, sem):
     """
-    Fetch book cards from multiple pages concurrently.
+    Fetches book cards from multiple pages concurrently.
 
     Args:
         urls (list): A list of URLs to scrape.
-        sem (asyncio.Semaphore): Semaphore to limit concurrent requests.
+        sem (asyncio.Semaphore): Semaphore to control request concurrency.
 
     Returns:
-        list: A flattened list of book cards from all pages.
+        list: A combined list of book cards from all pages.
     """
     try:
-        logger.info("Fetching books cards")
+        logger.info("Fetching book cards from multiple pages...")
         async with aiohttp.ClientSession() as session:
             tasks = [fetch_book_cards_from_page(session, sem, url) for url in urls]
             results = await asyncio.gather(*tasks)
             return [book_card for page_results in results for book_card in page_results]
     except Exception as error:
-        logger.error(f"Unable to fetch book cards, {error}")
+        logger.error(f"Failed to fetch book cards: {error}")
         return []
 
 
 def extract_book_data(book_card):
     """
-    Extract title, rating, and price from a single book card.
+    Extracts title, rating, and price from a book card.
 
     Args:
-        book_card (BeautifulSoup): A BeautifulSoup object representing a single book card.
+        book_card (BeautifulSoup): A BeautifulSoup object representing a book card.
 
     Returns:
-        tuple: A tuple containing the book's title (str), rating (int), and price (str).
+        tuple: (title (str), rating (int), price (float)).
     """
     title = book_card.find_all("a")[1].text.strip()
     rating = book_card.find("p", class_="star-rating")["class"][1]
-    price = book_card.find("p", class_="price_color").text.strip("£")
+    price = float(book_card.find("p", class_="price_color").text.strip("£"))
 
     rating_map = {"One": 1, "Two": 2, "Three": 3, "Four": 4, "Five": 5}
-    rating = rating_map.get(rating, 0)
-
-    return title, rating, price
+    return title, rating_map.get(rating, 0), price
 
 
 def write_books_to_csv(book_cards, filename):
     """
-    Write the extracted book data to a CSV file.
+    Writes extracted book data to a CSV file.
 
     Args:
-        book_cards (list): A list of book cards to process.
-        filename (str): The name of the CSV file to write the data to.
-
-    Raises:
-        TypeError: If there's an issue writing the data to the CSV file.
+        book_cards (list): A list of book cards containing extracted book data.
+        filename (str): The name of the output CSV file.
     """
-    logger.info(f"Writing {len(book_cards)} book records to file {filename}")
+    logger.info(f"Writing {len(book_cards)} book records to {filename}")
 
     try:
         with open(filename, "w", newline="", encoding="utf-8") as file:
@@ -179,23 +171,20 @@ def write_books_to_csv(book_cards, filename):
             for book_card in book_cards:
                 writer.writerow(extract_book_data(book_card))
 
-        logger.info(
-            f"Successfuly wrote {len(book_cards)} book records to file {filename}"
-        )
-
+        logger.info(f"Successfully wrote {len(book_cards)} book records to {filename}")
     except Exception as error:
-        logger.error(f"Unable to write to csv file, {error}")
+        logger.error(f"Failed to write to CSV file {filename}: {error}")
 
 
 async def main():
     """
-    Main function to coordinate the scraping and writing of book data.
+    Coordinates the entire web scraping process:
 
-    1. Generates URLs.
-    2. Fetches book data from multiple pages concurrently.
-    3. Writes the book data to a CSV file.
+    1. Generates URLs for book pages.
+    2. Fetches book data from all pages concurrently.
+    3. Writes the scraped data to a CSV file.
     """
-    logger.info("Web scrapper started")
+    logger.info("Web scraper started.")
     urls = await get_urls()
     sem = asyncio.Semaphore(10)
     book_cards = await fetch_all_book_cards(urls, sem)
